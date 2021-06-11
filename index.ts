584cd5d1b9cfbff7f2fb21f5f6285317a036c70b
Eduardo Mazolini <eduardo.mazolini@gmail.com>
2021-06-11 20:53:59 -0300

    Venom não vai pro THEN se esperar o login, ai não tem como enviar o qrcode

--- index.ts
+++ index.ts
@@ -3,14 +3,34 @@ import express from 'express';
 import { create } from 'venom-bot';
 import { catchQR, statusFind, start } from './venom/route';
 import { routeBuilder } from './express/route';
+import { defaultOptions, CreateConfig } from 'venom-bot/dist/config/create-config';
+import { puppeteerConfig } from 'venom-bot/dist/config/puppeteer.config';
 
 const app = express();
 const PORT = config.api.port;
 app.use(express.json());
 app.use(express.urlencoded());
 
-create(config.sessionName, catchQR, statusFind)
+create(config.sessionName, catchQR, statusFind,<CreateConfig>{
+  folderNameToken: 'tokens',
+  mkdirFolderToken: '',
+  headless: false, //Default: true
+  devtools: false,
+  useChrome: true,
+  debug: false,
+  logQR: true,
+  browserWS: '',
+  browserArgs: puppeteerConfig.chromiumArgs,
+  puppeteerOptions: {},
+  disableSpins: true, //Default: false
+  disableWelcome: true, //Default: false
+  updatesLog: true,
+  autoClose: 120000,
+  createPathFileToken: true,
+  waitForLogin: false, //Default: true
+})
     .then((client) => {
+        console.log("@@@@@@@@@@@@@@@@@@@@@@VENOM.THEN@@@@@@@@@@@@@@@@@");
         start(client);
 
         app.use("/"+config.sessionName,routeBuilder(client));
